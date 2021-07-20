59142e550f67cfaf0815c05db60b73c099bd2a6c
fumi <nihei.fumio.0706@gmail.com>
2021-07-20 16:57:43 +0900

    replace url by script 3

--- wiki/replace.sh
+++ wiki/replace.sh
@@ -3,3 +3,5 @@
 uri="https:\/\/github.com\/FumioNihei\/Test\/wiki\/"
 
 cat ./wiki/README.md | sed -e "s/.\/contents\//$uri/g" > ./wiki/_Sidebar.md
+cat ./wiki/_Sidebar.md | sed -e "s/.md//g" > ./wiki/_Sidebar.md
+
